5ef912f2a2d30f91aea862815738b4b320036204
ALDono <aubreyldono@gmail.com>
2024-04-08 15:49:47 +0100

    Big ol commit

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+const navButton = document.getElementById("navButton");
+const navButtonImage = document.getElementById("navButtonImage");
+const navLinks = document.getElementById("navLinks");
+
+let navOpen = false;
+navButton.addEventListener("click",function(){
+  if(navOpen === false){
+    navOpen = true;
+    navLinks.style.display = "flex";
+    navButtonImage.src = "images/cross_green.png"
+  }else{
+    navOpen = false;
+    navLinks.style.display = "none";
+    navButtonImage.src = "images/Hamburger_icon_green.png"
+  }
+})
